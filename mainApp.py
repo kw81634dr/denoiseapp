2069cea61be07a26cf199ac7dbb0c0bc605093a2
kevinwang <kw@mail.com>
2020-07-13 05:55:21 +0800

    完成單張dcm開啟功能

--- mainApp.py
+++ mainApp.py
@@ -1,67 +1,154 @@
 import sys
 
 #   Import Qt Libraries
-from PyQt5.QtWidgets import QMainWindow, QApplication, QFileDialog, QAction, QDirModel, QStyle, QToolBar, qApp
-from PyQt5.QtGui import QKeySequence
-from PyQt5.QtCore import Qt
-
+from PyQt5.QtWidgets import QMainWindow, QApplication, QFileDialog, QAction, QDirModel, QStyle, QToolBar, qApp, QDesktopWidget, QMessageBox
+from PyQt5.Qt import QStandardItemModel, QStandardItem
+from PyQt5.QtGui import QKeySequence, QFont, QColor
+from PyQt5.QtCore import Qt, QThread, pyqtSignal
 #   Import app UI Module
 from appUI import *
-
 #   Import other Libraries
 from pathlib import Path
 import cv2
-import pydicom
+from pydicom.misc import is_dicom
+from dicomIO import *
+
+
+# class PBarThreadClass(QThread):
+#     sender = pyqtSignal(int)
+#
+#     def __int__(self):
+#         super(PBarThreadClass, self).__init__()
+#
+#     def run(self):
+#         cnt = 0
+#         while cnt < 100:
+#             cnt = cnt+1
+#             time.sleep(0.3)
+#             self.sender.emit(cnt)   # 迴圈完畢後發出訊號
+
+class StandardItem(QStandardItem):
+    def __init__(self, txt='', font_size=12, set_bold=False, color=QColor(0, 0, 0)):
+        super().__init__()
+        fnt = QFont('Arial', font_size)
+        fnt.setBold(set_bold)
+        self.setEditable(False)
+        self.setForeground(color)
+        self.setFont(fnt)
+        self.setText(txt)
 
 
 class MyMainWindow(QMainWindow, Ui_MainWindow):
 
     working_path = None
+    files = []
 
     def __init__(self, parent=None):
         super(MyMainWindow, self).__init__(parent)
         self.ui = Ui_MainWindow()
         self.setupUi(self)
-
         self.setWindowIcon(QApplication.style().standardIcon(QStyle.SP_TitleBarMenuButton))
         self.setWindowTitle('KW De-noise App')
         self.statusBar().showMessage('Status Here')
+        # KW Custom UI Marco
+        self.make_app_in_screen_center()
+        self.init_toolbar()
 
+    def make_app_in_screen_center(self):
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+        print('Position to screen Center!!')
+
+    def init_toolbar(self):
         # Init ToolBar
         toolbar = QToolBar("My main toolbar")
         self.addToolBar(toolbar)
         toolbar.setContextMenuPolicy(Qt.PreventContextMenu)  # prevent users from hiding toolbar
         toolbar.setToolButtonStyle(Qt.ToolButtonTextUnderIcon)
-
         #   Actions
-        action_open_folder = self.actionOpen_Folder
-        action_open_folder.triggered.connect(self.open_file)
+        action_opnDcm = self.actionOpen_DICOM_File
+        action_opnDcm.triggered.connect(self.open_one_dcm)
         action_exit_app = self.actionExit
         action_exit_app.triggered.connect(qApp.quit)    # terminate App
-        toolbar.addAction(action_open_folder)
+        action_ScanDcmDir = self.actionScan_Dcm_From_Folder
+        action_ScanDcmDir.triggered.connect(self.scan_for_dcm)
 
+        # Add action to ToolBar
+        toolbar.addAction(action_opnDcm)
+        toolbar.addAction(action_ScanDcmDir)
         # Set Icon
-        action_open_folder.setIcon(QApplication.style().standardIcon(QStyle.SP_DirOpenIcon))
-
+        action_opnDcm.setIcon(QApplication.style().standardIcon(QStyle.SP_DirOpenIcon))
+        action_ScanDcmDir.setIcon(QApplication.style().standardIcon(QStyle.SP_FileDialogContentsView))
         # Set Short Cut
-        action_open_folder.setShortcut(QKeySequence.Open)
+        action_opnDcm.setShortcut(QKeySequence.Open)
 
-        # Tree-view File browser
-        self.model = QDirModel()
-        self.treeView.doubleClicked.connect(self.get_selected_item_path)
+        # Tree-view model
+        self.file_model = QDirModel()
+        self.dcm_model = QStandardItemModel()
+        self.dcm_model.invisibleRootItem()
+        '---------------------------------------------'
+        taiwan = StandardItem('Taiwan', 16, set_bold=True)
+        taipei = StandardItem('Taipei', 14, color=QColor(255, 110, 0))
+        banqiao = StandardItem('Banqiao', 12)
+        taiwan.appendRow(taipei)
+        taipei.appendRow(banqiao)
 
+        america = StandardItem('USA', 16, set_bold=True)
+        ca = StandardItem('CA', 14, color=QColor(0, 102, 255))
+        sanjose = StandardItem('San Jose', 12)
+        sanfrancisco = StandardItem('San Francisco', 12, color=QColor(0, 112, 31))
+        america.appendRow(ca)
+        ca.appendRow(sanjose)
+        ca.appendRow(sanfrancisco)
+
+        self.dcm_model.appendRow(taiwan)
+        self.dcm_model.appendRow(america)
+
+        # Tree-view define
+        self.treeView.setHeaderHidden(True)
+        # self.treeView.doubleClicked.connect(self.get_selected_item_path)
+        self.treeView.doubleClicked.connect(self.get_clicked_treeview_value)
+        self.treeView.setModel(self.dcm_model)
+        self.treeView.expandAll()
+
+    def scan_for_dcm(self):
+        p = QFileDialog.getExistingDirectory(self, "Choose Folder to Scan for DICOM")
+        if p != '':
+            fs = Path(p).rglob("*")
+            fl = []
+            for f in fs:
+                try:
+                    if is_dicom(f):
+                        fl.append(f)
+                except IOError:
+                    pass
+            print(*fl, sep='\n')
+
+
+    def get_clicked_treeview_value(self, val):
+        print(val.data())
+        print(val.row())
+        print(val.column())
+
+    # TREE VIEW
     def update_tree_view(self, signal):
-        # TREE VIEW
         self.treeView.setModel(self.model)
         self.treeView.setRootIndex(self.model.index(signal))
         self.treeView.show()
 
-    def open_file(self):
-        p = QFileDialog.getExistingDirectory(self, "Open")
-        if Path(p).is_dir():
-            file_path = p
-            self.update_tree_view(file_path)
-            # print('path=', file_path)
+    def open_one_dcm(self):
+        p = QFileDialog.getOpenFileName(self, 'choose DICOM File to Open', '', 'DICOM Image File (*.dcm)')[0]
+        if Path(p).is_file():
+            file_path = Path(p)
+            print('path=', file_path)
+            self.statusBar().showMessage('Reading DICOM File.')
+            dcm_ds, default_wl, default_ww, rescale_intercept, rescale_slope = read_dcm_file(file_path)
+            dcm_image_scaled = ct_windowed(dcm_ds, default_wl, default_ww, np.uint8, (0, 255))
+            image = cv2.cvtColor(dcm_image_scaled, cv2.COLOR_GRAY2RGB)
+            cv2.imshow('DICOM', image)
+            cv2.waitKey(0)
         else:
             self.statusBar().showMessage('Please choose Folder.')
 
